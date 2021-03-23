de2ae14ec31682c5b2492509510a4b1aa3576d5b
bqlin <bqlins@163.com>
2021-03-23 12:06:46 +0800

    Initial Commit

---
+++ CaptureDeviceInfo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CaptureDeviceInfo
+//
+//  Created by Bq Lin on 2021/3/23.
+//  Copyright © 2021 Bq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
